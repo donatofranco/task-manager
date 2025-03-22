0606140be77fb3b3ab98a7903152a854512ff29f
donatofranco <francoedonato@gmail.com>
2025-03-22 17:26:42 -0300

    Add CORS config

--- src/index.js
+++ src/index.js
@@ -3,15 +3,27 @@ const dotenv = require('dotenv');
 const userRoutes = require('./routes/user');
 const taskRoutes = require('./routes/task');
 const errorHandler = require('./middlewares/errorHandler');
+const helmet = require('helmet');
+const cors = require('cors');
 
 // Cargar variables de entorno desde .env
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 const HOST = '0.0.0.0';
+const corsOptions = {
+  origin: ['http://localhost:3001'], // frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  // credentials: true, // si necesitás enviar cookies o headers de autenticación
+  optionsSuccessStatus: 200, // Para asegurar que responda OK al preflight
+};
+
 
 // Middlewares
+app.use(helmet()); // Agrega headers de seguridad
+app.use(cors(corsOptions));  // Controla quién puede hacer requests a tu API
 app.use(express.json()); // Parsear JSON en los body requests
 app.use('/api/users', userRoutes);  // Rutas de usuarios
 app.use('/api/tasks', taskRoutes);
